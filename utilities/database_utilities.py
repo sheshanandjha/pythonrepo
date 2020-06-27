49c13b06e6779845da4dc996ddaceaaa76213367
Shesh Anand <sheshanand@local>
2020-06-27 05:42:29 +0100

    initial message

---
+++ utilities/database_utilities.py
@@ -0,0 +1,8 @@
+def create_database(database_path: str):
+    # TODO: generate the database
+    pass
+
+
+def save_words_to_database(database_path: str, words_list: str):
+    # TODO: save to database
+    pass
